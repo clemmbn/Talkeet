628f98583be2526a9b758db1329d1d043411951d
clemmbn <clem.mbn@gmail.com>
2026-05-01 10:21:46 +0200

    Xcode files

---
+++ frontend/Talkeet/TalkeetTests/TalkeetTests.swift
@@ -0,0 +1,18 @@
+//
+//  TalkeetTests.swift
+//  TalkeetTests
+//
+//  Created by Clément Maubon on 29/04/2026.
+//
+
+import Testing
+
+struct TalkeetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
